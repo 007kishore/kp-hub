32071da72fe25c53194bcb177977992195803581
KishorePatra <kishore.patra@afixi.com>
2016-02-24 11:23:08 +0530

    modified file

--- newone.php
+++ newone.php
@@ -4,4 +4,10 @@ echo "this is a testing file intended to practise of using github";
 echo "By this I will be comfortable with using git and github";
 echo "If i use it daily then it will be simple for me and I will be more familiar with this.";
 
+
+
+
+
+
+echo "I got some git commandto use";
 ?>
